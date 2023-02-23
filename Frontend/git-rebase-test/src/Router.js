e6231697617d6b1644efabb5f134274421166c9f
shinheylynn <heylub@gmail.com>
2023-02-23 14:18:50 +0900

    Add: login 페이지 완료

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -7,8 +7,13 @@ export default function Router() {
 	return (
 		<BrowserRouter>
 			<Routes>
+<<<<<<< HEAD
 				<Route path="/main" element={<Main />} />
 				<Route path="/login" element={<Login />} />
+=======
+				<Route path="/" element={<Main />} />
+				<Route path="/signin" element={<Login />} />
+>>>>>>> 1eaf5b4 (Add: login 페이지 완료)
 			</Routes>
 		</BrowserRouter>
 	);
